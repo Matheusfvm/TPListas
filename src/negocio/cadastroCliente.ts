22508a0c5ff6365a61cce388257b3555ef300e6b
Bruno Denardo <brunoden.bd@gmail.com>
2023-05-21 08:36:48 -0300

    Correção da data de emissão do RG

--- src/negocio/cadastroCliente.ts
+++ src/negocio/cadastroCliente.ts
@@ -42,10 +42,10 @@ export default class CadastroCliente extends Cadastro {
         while (temRG){
             let valorRG = this.entrada.receberTexto(`Por favor informe o número do RG: `);
             let dataRG = this.entrada.receberTexto(`Por favor informe a data de emissão do RG, no padrão dd/mm/yyyy: `);
-            let partesDataRG = data.split('/');
-            let anoRG = new Number(partesData[2].valueOf()).valueOf();
-            let mesRG = new Number(partesData[1].valueOf()).valueOf();
-            let diaRG = new Number(partesData[0].valueOf()).valueOf();
+            let partesDataRG = dataRG.split('/');
+            let anoRG = new Number(partesDataRG[2].valueOf()).valueOf();
+            let mesRG = new Number(partesDataRG[1].valueOf()).valueOf();
+            let diaRG = new Number(partesDataRG[0].valueOf()).valueOf();
             let dataEmissaoRG = new Date(anoRG, mesRG, diaRG); 
             let umRG = new RG(valorRG, dataEmissaoRG)
             todosRG.push(umRG)
